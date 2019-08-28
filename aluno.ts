7d550e73e019a809a2c284dd90277f7af05d864f
am33zhick <am33zhick@gmail.com>
2019-08-28 20:15:19 -0300

    criação da classe aluno

---
+++ aluno.ts
@@ -0,0 +1,11 @@
+class Aluno {
+
+    //nome : string   // sem essa linha basta adicionar ao construtor public antes de nome dentro de ()e apagar a linha de baixo
+    constructor(public nome:string){
+       // this.nome = nome
+    }
+       exibir(){
+           console.log("nome:"+ this.nome)
+       }
+    
+}
